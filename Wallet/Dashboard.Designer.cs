7eb86b369790498ef7a7a2de5cdfd1e90e79e722
MalauD <malaurydutour@outlook.fr>
2018-01-31 21:16:01 +0100

    Add files via upload

---
+++ Wallet/Dashboard.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Wallet
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Connect = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 864);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1242, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(908, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(334, 864);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Connect
+            // 
+            this.Connect.Location = new System.Drawing.Point(12, 12);
+            this.Connect.Name = "Connect";
+            this.Connect.Size = new System.Drawing.Size(113, 23);
+            this.Connect.TabIndex = 2;
+            this.Connect.Text = "Connect to nodes";
+            this.Connect.UseVisualStyleBackColor = true;
+            this.Connect.Click += new System.EventHandler(this.Connect_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(36, 64);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(234, 186);
+            this.listBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(232, 290);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(51, 290);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1242, 886);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.Connect);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Dashboard";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Dashboard_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button Connect;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Wallet/Dashboard.cs
@@ -0,0 +1,386 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BlockChainLibrary;
+using static BlockChainLibrary.Block;
+using static BlockChainLibrary.BlockChain;
+using static BlockChainLibrary.Block.Ledger;
+using static BlockChainLibrary.MessageUDP;
+using static BlockChainLibrary.Command;
+using static BlockChainLibrary.Function;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Wallet
+{
+    public partial class Dashboard : Form
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        private void InitClientNode()
+        {
+
+
+        }
+        static string trackerIp = "192.168.1.235";
+        static int trackerPort = 1114;
+
+        private static Socket _clientSocket;
+
+        private void ConnectToTracker()
+        {
+
+            toolStripStatusLabel1.Text = "Try to connnect to the tracker: " + trackerIp;
+
+            IPEndPoint ip = new IPEndPoint(IPAddress.Parse(trackerIp), trackerPort);
+
+            _clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                _clientSocket.BeginConnect(ip, new AsyncCallback(ConnectCallBack), null);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Tracker Exception : " + e);
+
+            }
+            
+
+            SendCommand(new Command(2));
+
+
+        }
+
+        static byte[] recbuffer = new byte[20000];
+        static Thread recevoir;
+
+        private void ConnectCallBack(IAsyncResult ar)
+        {
+            _clientSocket.EndConnect(ar);
+            Invoke((MethodInvoker)delegate
+            {
+                toolStripStatusLabel1.Text = "Connected to tracker " + trackerIp;
+                Connect.Enabled = false;
+
+            });
+
+            recevoir = new Thread(new ThreadStart(Receive));
+            recevoir.Start();
+        }
+
+
+        private void Receive()
+        {
+            
+            try
+            {
+                _clientSocket.BeginReceive(recbuffer, 0, recbuffer.Length, SocketFlags.None,
+                    new AsyncCallback(ReceiveCallback), null);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Tracker Exception : " + e);
+
+            }
+        }
+        bool acceptNode = true;
+
+        private void ReceiveCallback(IAsyncResult ar)
+        {
+            int ren = _clientSocket.EndReceive(ar);
+
+            if (acceptNode)
+            {
+                acceptNode = false;
+                AcceptNodes();
+            }
+            
+
+            MessageUDP msg = new MessageUDP()
+            {
+                Data = recbuffer
+            };
+
+            //Console.WriteLine(Encoding.Default.GetString(msg.Data));
+
+            Object obj = Function.Deserialize(msg);
+            ProcessMessage(obj);
+
+            try
+            {
+                _clientSocket.BeginReceive(recbuffer, 0, recbuffer.Length, SocketFlags.None,
+                    new AsyncCallback(ReceiveCallback), null);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Tracker Exception : " + e);
+
+            }
+
+           
+
+        }
+
+
+
+        static private void SendCommand(Object Order)
+        {
+            var msg = Function.Serialize(Order);
+            try
+            {
+                _clientSocket.BeginSend(msg.Data, 0, msg.Data.Length, SocketFlags.None, new AsyncCallback(SendCallback), null);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Tracker Exception : " + e);
+
+            }
+            
+
+        }
+        private static void SendCallback(IAsyncResult ar)
+        {
+            _clientSocket.EndSend(ar);
+        }
+
+
+
+
+
+        static List<IPEndPoint> nodeList = new List<IPEndPoint>();
+
+
+
+        private void ProcessMessage(object objectReceived)
+        {
+
+            if (objectReceived is List<IPEndPoint>)
+            {
+
+                nodeList = objectReceived as List<IPEndPoint>;
+                Invoke((MethodInvoker)delegate
+                {
+                    toolStripStatusLabel1.Text = "Node list received";
+                    listBox1.Items.Clear();
+                    foreach (var node in nodeList)
+                    {
+                        listBox1.Items.Add(node.Address.ToString());
+                        node.Port = nodePort;
+                    }
+                });
+                if (init)
+                {
+                    init = false;
+                    ConnectToNodes();
+                }
+                else
+                {
+
+                }
+            }        
+            
+        }
+
+        bool init = true;
+
+        private void Dashboard_Load(object sender, EventArgs e)
+        {
+            InitClientNode();
+        }
+
+        private void Connect_Click(object sender, EventArgs e)
+        {
+            ConnectToTracker();
+        }
+
+        int nodePort = 1120;
+
+        List<Socket> nodes = new List<Socket>();
+
+        private void ConnectToNodes()
+        {
+            foreach (var ip in nodeList)
+            {   
+                
+
+                Socket sc = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                var v = false;
+                foreach(var s in nodes)
+                {
+                    if(sc == s)
+                    {
+                        v = true;
+                    }
+                }
+                if (v)
+                    continue;
+
+                try
+                {
+                    sc.BeginConnect(ip, new AsyncCallback(NodeConnectCallBack), null);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Connection to node Exception : " + e);
+
+                }
+
+                
+                nodes.Add(sc);
+            }
+            Invoke((MethodInvoker)delegate
+            {
+                toolStripStatusLabel1.Text = "Connected to All node ";
+            });
+            
+        }
+
+        Socket nodesSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        const int BUFFER_SIZE = 10000;
+        private byte[] buffer = new byte[BUFFER_SIZE];
+
+        private void AcceptNodes()
+        {
+            try
+            {
+                nodesSocket.Bind(new IPEndPoint(IPAddress.Any, nodePort));
+                nodesSocket.Listen(0);
+                nodesSocket.BeginAccept(AcceptNodesCallback, null);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Accept to node Exception : " + e);
+
+            }
+
+            
+        }
+
+        private void AcceptNodesCallback(IAsyncResult ar)
+        {
+            Socket socket;
+            
+
+            try
+            {
+                socket = nodesSocket.EndAccept(ar);
+                socket.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveNodesCallback, socket);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Accept node Exception : " + e);
+
+            }
+            
+        }
+
+
+        private void ReceiveNodesCallback(IAsyncResult ar)
+        {
+            Socket current = (Socket)ar.AsyncState;
+            int received;
+            try
+            {
+                received = current.EndReceive(ar);
+            }
+            catch (SocketException)
+            {
+                current.Close();
+                nodes.Remove(current);
+
+                return;
+            }
+            MessageUDP msg = new MessageUDP()
+            {
+                Data = buffer
+            };
+            Object obj = Function.Deserialize(msg);
+            ProcessNodesMessage(obj, current);
+            try
+            {
+                current.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallback, current);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Receive from node Exception : " + e);
+
+            }
+
+            
+
+        }
+
+        private void ProcessNodesMessage(object obj, Socket current)
+        {
+            if (obj is String)
+            {
+                string s = obj as String;
+            
+            Invoke((MethodInvoker)delegate
+            {
+               listBox2.Items.Add(s);
+            });
+            
+            }
+        }
+
+        private void NodeConnectCallBack(IAsyncResult ar)
+        {
+            try
+            {
+                _clientSocket.EndConnect(ar);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("End connect node Exception : " + e);
+
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (var node in nodes)
+            {
+                SendCommand(textBox1.Text, node);
+            }
+        }
+        static private void SendCommand(Object Order, Socket socket)
+        {
+            var msg = Function.Serialize(Order);
+            //socket.BeginSend(msg.Data, 0, msg.Data.Length, SocketFlags.None, new AsyncCallback(SendNodesCallback), null);
+            socket.Send(msg.Data, 0, msg.Data.Length, SocketFlags.None);
+        }
+
+        private static void SendNodesCallback(IAsyncResult ar)
+        {
+            Socket socket = (Socket)ar.AsyncState;
+            
+            try
+            {
+                socket.EndSend(ar);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Receive from node Exception : " + e);
+
+            }
+        }
+    }
+}
